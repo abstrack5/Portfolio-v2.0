380c9d62cb4dd5e2c286aa87925876d97b00e528
Abstrack5 <armon_ah05@outlook.com>
2022-07-22 08:18:57 -0400

    complication with spinner, parent child. added background to homepage.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import "./App.css";
 import NavigationBar from "./components/Navbar/navbar";
 import Homepage from "./components/Home/home";
 
 function App() {
   return (
-    <div>
+    <div className=''>
       <div>
         <NavigationBar />
       </div>
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./home.css";
 import profilePic from "../../images/profilePic.jpeg";
+// import background from "../../images/background.jpg";
 
 function Homepage() {
   return (
-    <div className="home-container d-flex">
+    <div className="home-container background">
       <div>
-        <h1>Welcome, <br />My name is Armon Ahmadi</h1>
+        <h1>Hey!</h1>
+        <h1>I'm Armon</h1>
       </div>
       <div>
-        <img src={profilePic} alt="profile picture" />
+          <img src={profilePic} alt="profile picture" width='275px' className="profile-img"/>
       </div>
     </div>
   );
