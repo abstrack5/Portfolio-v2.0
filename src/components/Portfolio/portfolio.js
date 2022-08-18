d4ecb19e7dd055fd912cf5f9fcebc2e79db96353
Abstrack <Armon_ah05@outlook.com>
2022-08-17 22:18:02 -0400

    updated with most recent project

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./portfolio.css";
-import project1 from "../../images/coffee-br.png";
-import project2 from "../../images/GüdEats-app-screenshot.jpeg";
-import project3 from "../../images/weatherApp.png";
-import project4 from "../../images/run-buddy-snap.jpeg";
+
+import project1 from "../../images/lastProject.PNG";
+import project2 from "../../images/coffee-br.png";
+import project3 from "../../images/GüdEats-app-screenshot.jpeg";
+import project4 from "../../images/weatherApp.png";
 
 const Portfolio = () => {
   return (
@@ -13,22 +14,22 @@ const Portfolio = () => {
       </div>
       <div className="row pl-5">
         <div className=" col-6">
-          <a href="https://github.com/">
+          <a href="https://radiant-thicket-52137.herokuapp.com/">
             <img src={project1} className="port-img mb-2" alt='' />
           </a>
         </div>
         <div className="col-6">
-          <a href="https://github.com/">
+          <a href="https://limitless-everglades-24758.herokuapp.com/">
             <img src={project2} className="port-img mb-2 project-two" alt=''  />
           </a>
         </div>
         <div className="col-6">
-          <a href="https://github.com/">
+          <a href="https://miscmich.github.io/congenial-engine/">
             <img src={project3} className="port-img project-three" alt=''  />
           </a>
         </div>
         <div className="col-6">
-          <a href="https://github.com/">
+          <a href="https://abstrack5.github.io/Genius-Weather/">
             <img src={project4} className="port-img" alt=''  />
           </a>
         </div> as 
